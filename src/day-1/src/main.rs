e6df85cd3900f5dc2c87f6e374bb6b0e05a74028
Luke <lukewolf101010@gmail.com>
2022-12-01 22:36:55 -0700

    add comments and a few more tests, fix iter in case of less than 3 elves

--- src/day-1/src/main.rs
+++ src/day-1/src/main.rs
@@ -7,6 +7,17 @@ fn main() {
     println!("top 3 calories: {top_3_calories:?}");
 }
 
+/// A certain number of calories are passed in with the format
+/// 1000
+/// 2000
+///
+/// 3000
+/// 4000
+///
+/// Where elf 1 has calories 1000, and 2000 and elf 2 has calories 3000, and 4000
+///
+/// This function adds up the total calories per elf and returns that as a vector
+/// after sorting and then reversing it so that the highest numbers come first
 pub fn get_calories(numbers: &str) -> Option<Vec<i32>> {
     if numbers.len() == 0 {
         None
@@ -37,6 +48,7 @@ pub fn get_calories(numbers: &str) -> Option<Vec<i32>> {
     }
 }
 
+/// Gets the highest number of calories the elves are holding
 pub fn top_calories(numbers: &str) -> Option<i32> {
     if let Some(result) = get_calories(numbers) {
         Some(result[0])
@@ -45,9 +57,10 @@ pub fn top_calories(numbers: &str) -> Option<i32> {
     }
 }
 
+/// Gets the combined total of the number of calories the top 3 elves are holding
 pub fn top_3_calories(numbers: &str) -> Option<i32> {
     if let Some(result) = get_calories(numbers) {
-        Some(result[0..3].iter().fold(0, |accum, item| accum + item))
+        Some(result.iter().take(3).fold(0, |accum, item| accum + item))
     } else {
         None
     }
@@ -85,5 +98,15 @@ mod test {
     #[test]
     fn add_top_3_calories() {
         assert_eq!(top_3_calories("100\n\n200\n\n300"), Some(600))
+    }
+
+    #[test]
+    fn add_top_2_calories() {
+        assert_eq!(top_3_calories("100\n\n200"), Some(300))
+    }
+
+    #[test]
+    fn add_top_calories() {
+        assert_eq!(top_3_calories("100"), Some(100))
     }
 }
